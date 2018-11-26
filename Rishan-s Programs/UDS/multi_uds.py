3057119d861dd9ff5029ef8a8124be66f670f565
Rishan123 <alok76in@gmail.com>
2018-11-26 21:45:41 +0000

    Update multi_uds.py

--- Rishan-s Programs/UDS/multi_uds.py
+++ Rishan-s Programs/UDS/multi_uds.py
@@ -17,35 +17,42 @@ mc_in2=7
 mc_in3=21                                       
 mc_in4=20
 # Dis is the maximum distance for any actions for UDS
-dis = 10
+dis = 8
 # sp is the speed of the motors
-sp = 0.2
+sp = 0.5
 # sl is the amount of sleep in the program
-sl = 0.3
+sl = 0.1
+log_enabled = False
 car = Robot(left=(mc_in1,mc_in2),right=(mc_in3,mc_in4))
 car.stop()
 log = open("log.txt","w")
 def my_endfunc(sig, frame):
         print('You pressed Ctrl+C!')
         log.close()
+        car.stop()
         sys.exit(0)
 signal.signal(signal.SIGINT, my_endfunc)
 while True:
+    print("Getting Right")
     r = mypkg.calc_dist_cm(trig_pin_r, echo_pin_r)
+    print("Getting Left")
     l = mypkg.calc_dist_cm(trig_pin_l, echo_pin_l)
+    print("Getting Middle")
     m = mypkg.calc_dist_cm(trig_pin_m, echo_pin_m)
+    print("Comparing")
     b = mypkg.compare(l,m,r)
     print(' l= ',l," m = ",m," r = ",r)
-    log.write(time.asctime())
-    log.write(' == ')
-    log.write(str(l))
-    log.write(' ')
-    log.write(str(m))
-    log.write(' ')
-    log.write(str(r))
-    log.write('\n')
+    if log_enabled == True:
+            log.write(time.asctime())
+            log.write(' == ')
+            log.write(str(l))
+            log.write(' ')
+            log.write(str(m))
+            log.write(' ')
+            log.write(str(r))
+            log.write('\n')
     print(b)
-    if m > dis:
+    if m > dis and l > dis and r > dis:
         car.forward(speed=sp)
         print('Forward')
     else:
