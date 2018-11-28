f0ab20008ffb47aea85ffbc224bb7fe9337a5142
Rishan123 <alok76in@gmail.com>
2018-11-28 07:45:26 +0000

    Add files via upload

---
+++ Rishan-s Programs/My Packages/mypkg/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='mypkg',
+      version='0.3',
+      description='a library for CERCBot for Pi Wars',
+      url='to be done',
+      author='Rish the fish',
+      author_email='rish@fish.com',
+      license='Daddy',
+      packages=['mypkg'],
+      zip_safe=False)
