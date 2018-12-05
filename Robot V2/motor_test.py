b65f30ff521d1e973e0d41d638781728331fb153
andrewfell <45041069+andrewfell@users.noreply.github.com>
2018-12-05 21:21:13 +0000

    Add files via upload

--- Robot V2/motor_test.py
+++ Robot V2/motor_test.py
@@ -1,20 +1,25 @@
-from gpiozero import Robot
-from time import sleep
-
-robot = Robot(left=(8, 7), right=(21, 20))
-
-
-#this program tests that you have the GPIO mapping correct
-#the gpiozero library is used
+#This program tests that you have the GPIO mapping correct on your CERC Robot V2 chassis
+#refer to GPIO pins allocations here: https://www.raspberrypi.org/documentation/usage/gpio/
+#the 'robot' class of the gpiozero library is used below: https://gpiozero.readthedocs.io/en/stable/recipes.html#robot
 #the robot should first move forward, then spin right, then spin left, then go backwards.
 #if this works as expected, your GPIO mapping and motor driver board wiring is correct
 
-robot.forward()
-sleep(0.5)
-robot.right()
-sleep(0.5)
-robot.left()
-sleep(0.5)
-robot.backward()
-sleep(0.5)
-robot.stop()
+
+#first of all, import the Robot class from the 'gpiozero' library
+from gpiozero import Robot
+#then import the 'sleep' class from the 'time' library
+from time import sleep
+
+#define a robot, with the GPIO pin mapping as per the GPIO in the RobotV2.md file
+robot = Robot(left=(8, 7), right=(21, 20)) # dont change this pin mapping, otherwise your robot will be different to the others!
+
+
+robot.forward() #go forward indefinitely
+sleep(0.5) # sleep for 0.5seconds
+robot.right()  #spin right indefinitely
+sleep(0.5) #sleep for 0.5seconds
+robot.left() #spin left indefinitely
+sleep(0.5) #sleep for 0.5seconds
+robot.backward() #go backwards indefinitely
+sleep(0.5) #sleep for 0.5seconds
+robot.stop() #stop!
