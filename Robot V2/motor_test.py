0650b88a361ab3e6bdcab1731ba38088f38b93ae
andrewfell <45041069+andrewfell@users.noreply.github.com>
2018-12-05 21:22:55 +0000

    Add files via upload

--- Robot V2/motor_test.py
+++ Robot V2/motor_test.py
@@ -10,16 +10,16 @@ from gpiozero import Robot
 #then import the 'sleep' class from the 'time' library
 from time import sleep
 
-#define a robot, with the GPIO pin mapping as per the GPIO in the RobotV2.md file
-robot = Robot(left=(8, 7), right=(21, 20)) # dont change this pin mapping, otherwise your robot will be different to the others!
+#define a robot (called Burt :-) ), with the GPIO pin mapping as per the GPIO in the RobotV2.md file
+burt_the_robot = Robot(left=(8, 7), right=(21, 20)) # dont change this pin mapping, otherwise your robot will be different to the others!
 
 
-robot.forward() #go forward indefinitely
+burt_the_robot.forward() #go forward indefinitely
 sleep(0.5) # sleep for 0.5seconds
-robot.right()  #spin right indefinitely
+burt_the_robot.right()  #spin right indefinitely
 sleep(0.5) #sleep for 0.5seconds
-robot.left() #spin left indefinitely
+burt_the_robot.left() #spin left indefinitely
 sleep(0.5) #sleep for 0.5seconds
-robot.backward() #go backwards indefinitely
+burt_the_robot.backward() #go backwards indefinitely
 sleep(0.5) #sleep for 0.5seconds
-robot.stop() #stop!
+burt_the_robot.stop() #stop!
