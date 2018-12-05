595aef2275f13dd2ff7ed481b727faf4e9cc28fd
andrewfell <45041069+andrewfell@users.noreply.github.com>
2018-12-05 21:32:57 +0000

    Add files via upload

--- Robot V2/motor_test.py
+++ Robot V2/motor_test.py
@@ -1,4 +1,4 @@
-#This program tests that you have the GPIO mapping correct on your CERC Robot V2 chassis
+#This program tests that you have the motor GPIO mapping correct on your CERC Robot V2 chassis
 #refer to GPIO pins allocations here: https://www.raspberrypi.org/documentation/usage/gpio/
 #the 'robot' class of the gpiozero library is used below: https://gpiozero.readthedocs.io/en/stable/recipes.html#robot
 #the robot should first move forward, then spin right, then spin left, then go backwards.
@@ -10,7 +10,7 @@ from gpiozero import Robot
 #then import the 'sleep' class from the 'time' library
 from time import sleep
 
-#define a robot (called Burt :-) ), with the GPIO pin mapping as per the GPIO in the RobotV2.md file
+#define a robot (it's called Burt! :-) ), with the GPIO pin mapping as per the GPIO in the RobotV2.md file
 burt_the_robot = Robot(left=(8, 7), right=(21, 20)) # dont change this pin mapping, otherwise your robot will be different to the others!
 
 
