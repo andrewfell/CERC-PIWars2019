a9c644d00fc4eb8f1c2d6d281066368891c747b8
Rishan123 <alok76in@gmail.com>
2019-01-12 08:56:29 +0000

    Update detect_red.py
    Detecting colour red (only) is working!

--- Rishan-s Programs/Somewhere Over The Rainbow/detect_red.py
+++ Rishan-s Programs/Somewhere Over The Rainbow/detect_red.py
@@ -68,7 +68,7 @@ while True:
     print ("ru = ",ru)
     print ("gu = ",gu)
     print ("bu = ",bu)
-    if red < 1000 or green < 1000 or blue < 1000:
+    if red < 1000:
         print('Turning left')
     else:
         print('Forward')
